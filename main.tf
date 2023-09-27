bf265b694dbc92aa694c8e515a06c2e442b0075a
kanigirijayanth <kanigirijayanth@gmail.com>
2023-09-27 11:09:25 +0530

    updating provider block

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_security_group" "alb-ecs-tf-sg" {
   name        = "alb-ecs-tf-sg"
   description = "SG for ALB-ECS"
