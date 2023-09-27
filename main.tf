64547196c7ad5ac4aecb706aaa25455c4f7989b5
kanigirijayanth <kanigirijayanth@gmail.com>
2023-09-27 16:44:21 +0530

    only sg

--- main.tf
+++ main.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 
-resource "aws_security_group" "alb-ecs-tf-sg" {
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_security_group" "cicd-sg" {
   name        = "alb-ecs-tf-sg"
   description = "SG for ALB-ECS"
 
@@ -16,39 +19,6 @@ resource "aws_security_group" "alb-ecs-tf-sg" {
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-
-resource "aws_security_group" "ecs-tf-sg" {
-  name        = "ecs-tf-sg"
-  description = "SG for ECS"
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port       = 0
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb-ecs-tf-sg.id]
   }
 
   egress {
